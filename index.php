006255bc9cf81adfb4674fc0e38f228ca9c65705
Al Scott <thealscott@gmail.com>
2012-07-09 15:08:16 +0100

    added existing content

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html class="blue">
+    <head>
+        <link rel="stylesheet" type="text/css" href="assets/css/screen.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <h1>thealscott.com</h1>
+            </header>
+            <section class="content">
+                <div class="blue">
+                    <h2>Item 1</h2>
+                </div>
+                <div class="orange">
+                    <h2>Item 2</h2>
+                </div>
+                <div class="red double-width">
+                    <h2>Item 3</h2>
+                </div>
+                <div class="green">
+                    <h2>Item 1</h2>
+                </div>
+                <div class="orange">
+                    <h2>Item 1</h2>
+                </div>
+                <div class="red">
+                    <h2>Item 1</h2>
+                </div>
+                <div class="green">
+                    <h2>Item 2</h2>
+                </div>
+                <div class="orange double-width">
+                    <h2>Item 3</h2>
+                </div>
+                <div class="blue double-width">
+                    <h2>Item 4</h2>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
